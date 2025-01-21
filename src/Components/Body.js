8d406d167963f836bf9285cdd389ab3d12bbc937
Suvrat <suvratmittal007@gmail.com>
2025-01-21 22:51:53 +0530

    completed basics

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,53 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {db} from "../Utils/firebaseConfig";
-import { collection, addDoc } from "firebase/firestore";
+import {collection, addDoc, getDocs, deleteDoc, doc, updateDoc} from "firebase/firestore";
 import GetData from "./GetData";
 
 const Body = () => {
     const [todo,setTodo] = useState("");
+    const [data, setData] = useState([]);
+    const [isUpdating, setIsUpdating] = useState(false);
+    const [selectedTodo,setSelectedTodo] = useState(null);
+    const [updatedData, setUpdatedData] = useState("");
+
+    const getDataFromBase = async () =>{
+        const querySnapshot = await getDocs(collection(db , "todos"));
+        const newData = querySnapshot.docs.map((doc) => ({
+            ...doc.data(),id: doc.id,
+        }))
+        setData(newData);
+    }
+
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        getDataFromBase();
+    }, []);
+
+    // console.log(data);
+
+    const deleteElement = async (id) =>{
+        await deleteDoc(doc(db, "todos", id));
+        getDataFromBase();
+    }
+
+    const update = (todo) => {
+        setIsUpdating(true);
+        setSelectedTodo(todo);
+    };
+
+    const handleUpdate = async (id) => {
+        try{
+            const docRef = doc(db , "todos", id);
+            // console.log(docRef);
+            await updateDoc(docRef , {todo:updatedData});
+            setIsUpdating(false);
+            setUpdatedData("");
+            getDataFromBase();
+        }catch(error){
+            console.log(error)
+        }
+    }
     const handleClick = async (e) => {
         e.preventDefault();
 
@@ -12,6 +55,8 @@ const Body = () => {
             const docRef = await addDoc(collection(db,"todos"),{
                 todo: todo,
             })
+            getDataFromBase()
+
         }catch (error) {
             console.log(error);
         }
@@ -23,7 +68,30 @@ const Body = () => {
                        type="text"
                        placeholder="Enter Input" onChange={(e) => setTodo(e.target.value)}/>
                 <button onClick={handleClick} className="border-2 p-2"> Submit</button>
-                <GetData/>
+                <div>
+                    {data.map(todo => (
+                        <div className="flex flex-row">
+                            <p className="border-2 p-2 m-2 cursor-pointer"
+                               onClick={() => update(todo)}>{todo.todo}
+                            </p>
+                            <button className="border-2 p-2 m-2 cursor-pointer"
+                                    onClick={() => deleteElement(todo.id)}>delete
+                            </button>
+                        </div>
+                    ))}
+                </div>
+                {
+                isUpdating && (
+                    <div>
+                        <input className="border-2 p-2 m-2 cursor-pointer"
+                               placeholder="New Data"
+                               onChange={e => setUpdatedData(e.target.value)}/>
+                        <button onClick={() => handleUpdate(selectedTodo.id)}
+                                className="border-2 p-2 m-2 cursor-pointer">update
+                        </button>
+                    </div>)
+            }
+
             </div>
 
         </div>
